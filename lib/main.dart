a18d58d090107722b12516317e09712ff52e23f5
iuzul <iuzul@IBRAHIMUZUL>
2020-09-24 11:38:29 +0300

    85. Burc Detay Sayfasının Oluşturulması

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_burc_rehberi/burc_detay.dart';
-import 'package:flutter_burc_rehberi/burc_list.dart';
+import 'package:flutter_burc_rehberi/burc_liste.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,17 +11,27 @@ class MyApp extends StatelessWidget {
         title: "Burç Rehberi",
         debugShowCheckedModeBanner: false,
 
-        /*initialRoute: "/burcListesi",
+        initialRoute: "/burcListesi",
 
         routes: {
-          "/" : (context) => BurcListesi(),
-          "/burcListesi" : (context) => BurcListesi(),
-          "/burcDetay/123" : (context) => BurcDetay(),
-        },*/
+          "/": (context) => BurcListesi(),
+          "/burcListesi": (context) => BurcListesi(),
+        },
+
+        onGenerateRoute: (RouteSettings settings) {
+          List<String> pathElemanlari =
+              settings.name.split("/"); //  /  burcDetay  /    1
+          if (pathElemanlari[1] == 'burcDetay') {
+            return MaterialPageRoute(
+                builder: (context) => BurcDetay(int.parse(pathElemanlari[2])));
+          }
+          return null;
+        },
 
         theme: ThemeData(
           primarySwatch: Colors.pink,
         ),
-        home: BurcListesi());
+        //home: BurcListesi()
+     );
   }
 }

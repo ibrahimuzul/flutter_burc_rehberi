9d1de1542a15a9748aabf77e0939422d879b9146
iuzul <iuzul@IBRAHIMUZUL>
2020-09-24 10:53:48 +0300

    84. Burcların Listelenmesi

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_burc_rehberi/burc_detay.dart';
 import 'package:flutter_burc_rehberi/burc_list.dart';
 
-void  main()  =>  runApp(MyApp());
+void main() => runApp(MyApp());
 
-
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:"Burç Rehberi",
+        title: "Burç Rehberi",
         debugShowCheckedModeBanner: false,
-      theme:ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home:BurcListesi()
-    );
+
+        /*initialRoute: "/burcListesi",
+
+        routes: {
+          "/" : (context) => BurcListesi(),
+          "/burcListesi" : (context) => BurcListesi(),
+          "/burcDetay/123" : (context) => BurcDetay(),
+        },*/
+
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: BurcListesi());
   }
-  
 }
